ccb732403b606801a894d9aba2ee58943fb52e6b
jamesFernando170 <jamesfr8764@hotmail.com>
2021-11-20 17:16:15 -0500

    Agregando un formulario tanto para el login como para el recuperar-clave

--- src/app/public/template/navbar/navbar.component.ts
+++ src/app/public/template/navbar/navbar.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  session: boolean = false; //Variable que nos servira para ocultar el login o el logout, dependiendo del caso
+  
 
   constructor() { }
 
